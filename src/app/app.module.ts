729b3825d3bfa24b160720311ca6ec84032faf91
VyDee <vy.duong@cuw.edu>
2020-06-23 23:36:54 -0500

    routing basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,19 @@ import { PageNotFoundComponent } from './page-not-found.component';
 import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', component: PageNotFoundComponent }
+
+    ]),
     ProductModule,
     UserModule,
     MessageModule
