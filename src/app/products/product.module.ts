432dec276c2d365e9489cf62ac9165d09429e7e4
VyDee <vy.duong@cuw.edu>
2020-07-05 18:34:24 -0500

    activating child routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -7,6 +7,8 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { ProductResolver } from './product-resolver.service';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 
 @NgModule({
   imports: [
@@ -20,10 +22,25 @@ import { ProductResolver } from './product-resolver.service';
                                                   // the browser to prefetch the data before 
                                                   // launching the ProductDetailComponent
       },
-      { 
+      {
         path: 'products/:id/edit', 
         component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver } 
+        resolve: { resolvedData: ProductResolver }, 
+        children: [
+          {
+            path:'',
+            redirectTo:'info',
+            pathMatch: 'full'
+          },
+          {
+            path:'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path:'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
       }
 
     ])
@@ -31,7 +48,9 @@ import { ProductResolver } from './product-resolver.service';
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
