bcf5a1d59e5f533dbef3ed0444d999c841afdba0
VyDee <vy.duong@cuw.edu>
2020-07-11 16:50:14 -0500

    preload all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
@@ -10,12 +10,12 @@ import { AuthGuard } from './user/auth.guard';
             { path: 'welcome', component: WelcomeComponent},
             { 
                 path: 'products',
-                canLoad: [AuthGuard],
+                canActivate: [AuthGuard],
                 loadChildren: () => 
                     import('./products/product.module').then(m => m.ProductModule)},
             { path: '', redirectTo: 'welcome', pathMatch: 'full'},
             { path: '**', component: PageNotFoundComponent }
-          ]),
+          ], { preloadingStrategy: PreloadAllModules }),
     ],
     exports: [RouterModule]
 })
